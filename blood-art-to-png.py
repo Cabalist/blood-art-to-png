2cc92de631a7c2c1f15bb0ca20288bb27c72ed83
Ryan Jarvis <ryan@shopboxretail.com>
2018-12-05 22:46:37 -0800

    Convert to Python3.6+ with type hinting

--- blood-art-to-png.py
+++ blood-art-to-png.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Blood ART to PNG converter
@@ -8,50 +8,51 @@
 import os
 import struct
 import sys
+from typing import Dict, Tuple, Union
 
 import png
 
 from palette import palette
 
 
-def fatalError(e):
+def fatalError(e: Union[None, str, Exception]) -> None:
     if isinstance(e, str):
         sys.stderr.write(e)
         sys.exit(2)
 
     elif isinstance(e, IOError):
-        sys.stderr.write('I/O error({}): {}\n'.format(e.errno, e.strerror))
+        sys.stderr.write(f'I/O error({e.errno}): {e.strerror}\n')
         sys.exit(3)
 
     else:
-        sys.stderr.write('Error: {}\n'.format(sys.exc_info()[0]))
+        sys.stderr.write(f'Error: {sys.exc_info()[0]}\n')
         sys.exit(4)
 
 
 # ----------------------------------------------------------------------------
-def initInfo():
+def initInfo() -> dict:
     return {}
 
 
 # ----------------------------------------------------------------------------
-def addInfo(info, section, key, val):
+def addInfo(info: Dict[str, Dict[str, int]], section: str, key: str, val: int):
     if not section in info:
         info[section] = {}
     info[section][key] = val
 
 
-def writeInfo(filename, info):
-    name = os.path.splitext(filename)[0] + '.ini'
+def writeInfo(path: str, info: Dict[str, Dict[str, int]]) -> None:
+    name = os.path.splitext(path)[0] + '.ini'
     with open(name, 'w') as f:
         for section in sorted(info):
-            f.write('[{}]\n'.format(section))
+            f.write(f'[{section}]\n')
             for key in sorted(info[section]):
-                f.write('{}={}\n'.format(key[2:], info[section][key]))
+                f.write(f'{key[2:]}={info[section][key]}\n')
             f.write('\n')
 
 
-def extractPNG(raw_8bpp, xsize, ysize, extended, index, info):
-    name = 'tile{:06d}.png'.format(index)
+def extractPNG(raw_8bpp: Tuple[int], xsize: int, ysize: int, extended: int, index: int, info: Dict[str, Dict[str, int]]) -> None:
+    name = f'tile{index:06d}.png'
     args = struct.unpack('BbbB', struct.pack('<L', extended))
     raw_32bpp = []
     has_alpha = False
@@ -88,7 +89,7 @@ def extractPNG(raw_8bpp, xsize, ysize, extended, index, info):
     addInfo(info, name, '7.ext2', args[3])
 
 
-def unpackART(raw, info):
+def unpackART(raw: bytes, info: Dict[str, Dict[str, int]]) -> None:
     art_header = struct.unpack_from('<LLLL', raw)
     art_version = art_header[0]
     art_start = art_header[2]
@@ -96,16 +97,16 @@ def unpackART(raw, info):
     art_count = art_end - art_start + 1
 
     if art_version != 1:
-        fatalError('invalid ART version number: {}\n'.format(art_version))
+        fatalError(f'invalid ART version number: {art_version}\n')
 
     addInfo(info, 'header', '0.version', art_version)
     addInfo(info, 'header', '1.start', art_start)
     addInfo(info, 'header', '2.end', art_end)
     addInfo(info, 'header', '3.count', art_count)
 
-    tiles_xsizes = struct.unpack_from('<{:d}H'.format(art_count), raw, 16)
-    tiles_ysizes = struct.unpack_from('<{:d}H'.format(art_count), raw, 16 + art_count * 2)
-    tiles_extended = struct.unpack_from('<{:d}L'.format(art_count), raw, 16 + art_count * 4)
+    tiles_xsizes = struct.unpack_from(f'<{art_count:d}H', raw, 16)
+    tiles_ysizes = struct.unpack_from(f'<{art_count:d}H', raw, 16 + art_count * 2)
+    tiles_extended = struct.unpack_from(f'<{art_count:d}L', raw, 16 + art_count * 4)
 
     art_offset = 16 + art_count * (2 + 2 + 4)
 
@@ -113,16 +114,16 @@ def unpackART(raw, info):
         size = tiles_xsizes[i] * tiles_ysizes[i]
         if size == 0:
             continue
-        raw_8bpp = struct.unpack_from('{:d}B'.format(size), raw, art_offset)
+        raw_8bpp = struct.unpack_from(f'{size:d}B', raw, art_offset)
         art_offset = art_offset + size
         extractPNG(raw_8bpp, tiles_xsizes[i], tiles_ysizes[i], tiles_extended[i], art_start + i, info)
 
 
-def processFile(filename):
+def processFile(path: str) -> None:
     info = initInfo()
 
     try:
-        with open(filename, 'rb') as f:
+        with open(path, 'rb') as f:
             raw = f.read()
             unpackART(raw, info)
 
@@ -132,17 +133,17 @@ def processFile(filename):
     except:
         fatalError(None)
 
-    writeInfo(filename, info)
+    writeInfo(path, info)
 
 
 if __name__ == '__main__':
 
     if len(sys.argv) == 1:
-        print('usage: {} file1.art [file2.art] [...]'.format(sys.argv[0]))
+        print(f'usage: {sys.argv[0]} file1.art [file2.art] [...]')
         sys.exit(1)
 
     for filename in sys.argv[1:]:
-        print('processing file: {}'.format(filename))
+        print(f'processing file: {filename}')
         processFile(filename)
 
     sys.exit(0)
